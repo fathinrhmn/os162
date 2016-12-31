311fbb4a4ef5b110fd53592bfdd085da3d60e50b
Muhammad Fathin Rahman - 1406622944 <muhammad.fathin41@badak.cs.ui.ac.id>
2016-12-31 20:08:57 +0700

    rem uas babak 4

--- XREMEDY2/ambil.sh
+++ XREMEDY2/ambil.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
 # (c) 2016 Muhammad Fathin Rahman -- This is free software
+# REV02 Sat Dec 31 19:30:22 WIB 2016 - remove START and STOP berkas.
 # REV01 Thu Dec 29 21:45:28 WIB 2016 - remove "[DISK]"
 # REV00 Thu Dec 29 20:35:41 WIB 2016 - remove "ZCZC" and "NNNN"
 # START Thu Dec 29 15:11:21 WIB 2016
 # Script ini digunakan untuk ekstraksi halaman web http://os162.vlsm.org/2016/11/ranking-os162.html
 # Mengambil urutan ranking lalu disimpan ke dalam ranking.txt
 
-echo "========================================================== START BERKAS" > ranking.txt
-temp=`w3m -dump http://os162.vlsm.org/2016/11/ranking-os162.html | sed -n '/ZCZC/,/NNNN/p' | sed '/ZCZC/,+0 d' | sed '/NNNN/,+0 d' | sed '/DISK/,+0 d'`
-echo "$temp" >> ranking.txt
-echo "============================================================ STOP BERKAS" >> ranking.txt
+temp=`w3m -dump -cols 130 http://os162.vlsm.org/2016/11/ranking-os162.html | sed -n '/ZCZC/,/NNNN/p' | sed '/ZCZC/,+0 d' | sed '/NNNN/,+0 d' | sed '/DISK/,+0 d'`
+echo "$temp" > ranking.txt
---
+++ XREMEDY2/ambil2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# 2016 Muhammad Fathin Rahman -- This is free software
+# START Sat Dec 31 16:23:21 WIB 2016
+# Script ini digunakan untuk memeriksa isi URL tiap baris di berkas "ranking.txt"
+# Melakukan text dump url lalu text dimasukkan ke file .txt baru
+
